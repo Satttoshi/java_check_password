e7d4ec045c8f92dec74ab0ef40f16bdc5787f1b5
Josh <joshuastolle445@gmail.com>
2023-06-02 11:00:52 +0200

    add test and paswordContainsNumber check method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,4 +9,20 @@ public class Main {
         System.out.println("Please enter your password: ");
         String password = scanner.nextLine();
     }
+
+    public static boolean isInInputRange(int number) {
+        return number > 3 && number < 21;
+    }
+
+    public static boolean passwordContainsNumbers(String password) {
+
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isDigit(password.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
--- src/test/java/org/example/TestMain.java
+++ src/test/java/org/example/TestMain.java
@@ -1,5 +1,6 @@
+package org.example;
+
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -11,11 +12,24 @@ public class TestMain {
                     "3, false",
                     "4, true",
                     "12, true",
-                    "15, false"
+                    "21, false"
             }
     )
     public void testIsInInputRange(int number, boolean expected) {
         boolean actual = Main.isInInputRange(number);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "\"myCoolPassword\", false",
+                    "\"myCoolPassword123\", true",
+                    "\"c4tf1sh\", true",
+            }
+    )
+    public void testPasswordContainsNumbers(String password, boolean expected) {
+        boolean actual = Main.passwordContainsNumbers(password);
         Assertions.assertEquals(expected, actual);
     }
 
